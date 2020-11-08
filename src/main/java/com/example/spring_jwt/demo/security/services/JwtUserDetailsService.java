43aeeb3b8f53e3600cdcbf45b3cd8ea3ad8e92da
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-11-08 19:38:00 +0300

    Added jwt user details service

---
+++ src/main/java/com/example/spring_jwt/demo/security/services/JwtUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.example.spring_jwt.demo.security.services;
+
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if ("codeyourempire".equals(username)) {
+            return new User("codeyourempire", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+                    new ArrayList<>());
+        } else {
+            throw new UsernameNotFoundException("Cannot found user with name " + username);
+        }
+    }
+}
