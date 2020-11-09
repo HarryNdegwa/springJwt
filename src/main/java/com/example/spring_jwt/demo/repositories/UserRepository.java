0dfd2973682afd950e00370d98403dcfea534c35
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-11-09 23:01:58 +0300

    Added the User repository

---
+++ src/main/java/com/example/spring_jwt/demo/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.spring_jwt.demo.repositories;
+
+import com.example.spring_jwt.demo.models.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
