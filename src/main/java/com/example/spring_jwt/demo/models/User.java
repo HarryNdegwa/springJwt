04ddf68af8d357bd723a5dd36d032bf8ee116c04
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-11-09 21:08:54 +0300

    Added User entity

---
+++ src/main/java/com/example/spring_jwt/demo/models/User.java
@@ -0,0 +1,51 @@
+package com.example.spring_jwt.demo.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column
+    private String username;
+
+    @Column
+    @JsonIgnore
+    private String password;
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
