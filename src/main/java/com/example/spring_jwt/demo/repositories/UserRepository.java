cd6eecb2601ff4318480672b5263047ea4b4107a
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-11-10 14:21:56 +0300

    Added findByUsername user repo utility method

--- src/main/java/com/example/spring_jwt/demo/repositories/UserRepository.java
+++ src/main/java/com/example/spring_jwt/demo/repositories/UserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserDao, Long> {
-
+    UserDao findByUsername(String username);
 }
